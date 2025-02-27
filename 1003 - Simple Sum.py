70d3c046abc1075ba8ef4bde1bd9468389e616d2
theGabeson <gabethemighty@gmail.com>
2025-02-26 23:52:57 -0300

    Exercise 1003 added.

---
+++ 1003 - Simple Sum.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+sum = a+b
+print(f"SOMA = {sum}")
+
+#I could've done the sum directly on the print statement, but I prefer to do calculations
+# and other things away from the output of the code.
